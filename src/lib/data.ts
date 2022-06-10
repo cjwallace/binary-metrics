f54291f92ee4f43a10ca1111b23a262bd08c8e1d
Chris Wallace <chris@bitcollider.io>
2022-06-10 23:09:12 +0100

    Add realistic prediction data
    Include realistic (but not a real problem) prediction dataset created with scikit-learn's make_classification and a default logistic regression model. Also change random data generator to default to more instances.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 import type { Label, Prediction } from './metrics';
 
-const predictions = Array.from(Array(100).keys()).map((el) => el / 100);
-const labels: Label[] = Array.from(Array(100).keys()).map(() => (Math.random() > 0.6 ? 1 : 0));
+const predictions = Array.from(Array(10000).keys()).map((el) => el / 10000);
+const labels: Label[] = Array.from(Array(10000).keys()).map(() => (Math.random() > 0.6 ? 1 : 0));
 
 const defaultData: Prediction[] = predictions.map((p, i) => ({ probability: p, label: labels[i] }));
 
